587e8811794395d81c3d5a41eaa677d01eed158d
SujanaP07 <floydsujana@gmail.com>
2020-09-03 11:53:49 +0530

    First Commit

--- lib/ui/bottom_view.dart
+++ lib/ui/bottom_view.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
+import 'package:intl/intl.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart';
 import 'dart:core';
 import 'package:weather_app/model/forecast_model.dart';
 
@@ -11,8 +11,8 @@ Widget bottomView(AsyncSnapshot<ForecastModel>snapshot,BuildContext context){
  var forecastList=snapshot.data.list;
  return Container(
    child:Column(
-     mainAxisAlignment: MainAxisAlignment.start,
-     children:[
+       mainAxisAlignment: MainAxisAlignment.start,
+       children:[
        Text("5 DAY WEATHER FORECAST",
          textAlign: TextAlign.center,
          style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold),
@@ -20,9 +20,18 @@ Widget bottomView(AsyncSnapshot<ForecastModel>snapshot,BuildContext context){
        Container(
          padding:EdgeInsets.symmetric(vertical:16.0, horizontal:20.0),
          child:ListView.separated(
-           
+           separatorBuilder:(context,index)=>SizedBox(width:8.0),
+           itemCount:forecastList.length,
+           itemBuilder:(context,index)=>ClipRRect(
+             borderRadius:BorderRadius.all(Radius.circular(10.0)),
+             child:Container(
+               width:MediaQuery.of(context).size.width/2.7,
+               height:160.0,
+               child:ForecastCard(snapshot,index),
+             )
          )
-       )
+       ),
+       ),
      ]
    )
  );
